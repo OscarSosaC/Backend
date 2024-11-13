6a51dbbd647ea7aea336c984cd7c9f021cf157da
dahiana bermudez <c.dahianabp@gmail.com>
2024-11-13 00:13:50 -0500

    agregar registro y login

---
+++ src/main/java/com/auradecristal/aura_de_cristal/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.auradecristal.aura_de_cristal.auth;
+
+import com.auradecristal.aura_de_cristal.config.JwtService;
+import com.auradecristal.aura_de_cristal.entity.Role;
+import com.auradecristal.aura_de_cristal.entity.Usuario;
+import com.auradecristal.aura_de_cristal.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UsuarioRepository usuarioRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register (RegisterRequest request){
+        Usuario usuario = Usuario.builder()
+                .nombre(request.getNombre())
+                .apellido(request.getApellido())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .rol(Role.USER)
+                .build();
+        usuarioRepository.save(usuario);
+        String token = jwtService.generateToken(usuario);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        Usuario usuario = usuarioRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("No existe el usuario"));
+
+        String token = jwtService.generateToken(usuario);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
